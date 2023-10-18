7d95eec711be6e06ea4e3755aeee0c7066839fda
ziplenok <shivczov00@gmail.com>
2023-10-18 16:29:12 +0600

    pre-final

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,21 @@
 export default defineNuxtRouteMiddleware(() => {
   const user = useSupabaseUser();
+  // const client = useSupabaseClient();
+  // const currentProfile = ref();
+  // const { startLoading, stopLoading } = useSignInLoading();
+  // startLoading();
+  // async function getCurrentProfile() {
+  //   let { data, error } = await client
+  //     .from("profiles")
+  //     .select()
+  //     .eq("id", user.value.id);
+
+  //   if (error) console.log(error);
+  //   currentProfile.value = data[0];
+  //   stopLoading();
+  // }
+  // getCurrentProfile();
+
   if (!user.value) {
     return navigateTo("/login");
   }
